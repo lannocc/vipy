92124e920da5cc9e4d80ae84a5c26086df60b375
Shawn Wilson <shawn@lannocc.com>
2019-07-06 23:11:46 -0600

    Get terminal size.

--- ag/vipy/editor.py
+++ ag/vipy/editor.py
@@ -2,10 +2,18 @@
 # @%@~LICENSE~@%@
 
 from . import *
+from .terminalsize import get_terminal_size
+
+import time
+
 
 def main():
     print("here we go")
 
+    w,h = get_terminal_size()
+    print("width = {}, height = {}".format(w,h))
+
+    time.sleep(3)
     clear_screen()
 
     while True:
