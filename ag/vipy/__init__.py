14f692d93bbdf2da6fac52d584d48a2ca78222d6
Shawn Wilson <shawn@lannocc.com>
2019-07-06 22:34:59 -0600

    Beginning keyboard input.

--- ag/vipy/__init__.py
+++ ag/vipy/__init__.py
@@ -15,3 +15,35 @@ that (partially) mimics the vim editor.
 from .__version__ import __version__
 
 
+
+def _read_char():
+    import sys
+
+    try:
+        import termios
+
+    except ImportError:
+        # Non-POSIX; assume Windows
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX
+    import tty
+
+    def getch():
+        fd = sys.stdin.fileno()
+        settings = termios.tcgetattr(fd)
+
+        try:
+            tty.setraw(fd)
+            char = sys.stdin.read(1)
+
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, settings)
+
+        return char
+
+    return getch
+
+read_char = _read_char()
+
--- ag/vipy/editor.py
+++ ag/vipy/editor.py
@@ -1,7 +1,21 @@
 # Copyright (C) 2019 Alpha Griffin
 # @%@~LICENSE~@%@
 
+from . import read_char
 
 def main():
     print("here we go")
+
+    while True:
+        c = read_char()
+        i = ord(c)
+
+        print("i got it: {} = {}".format(i, c))
+
+        if i == 3: # ctrl c
+            break
+
+        elif i == 26: # ctrl z
+            # TODO: ask to save changes
+            break
 
